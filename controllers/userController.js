1a596ccfa613a335f0ccaeaeab908618a10baf38
Vivek <vivekraj78965.vr@gmail.com>
2023-12-22 14:21:53 +0530

    user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,23 @@
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
+// register user
 const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
     if (username && email && password) {
+      const salt = await bcrypt.genSalt(parseInt(process.env.SALT_CONSTANT));
+      const hashedPassword = await bcrypt.hash(password, salt);
       const userData = new User({
         username: username,
         email: email,
-        password: password,
+        password: hashedPassword,
       });
       await userData.save();
       res
         .status(201)
-        .json({ status: "success", message: "user registered successfully"});
+        .json({ status: "success", message: "user registered successfully" });
     } else {
       res
         .status(400)
@@ -21,21 +26,53 @@ const registerUser = async (req, res) => {
   } catch (error) {
     if (error.code === 11000) {
       res
-        .status(400)
+        .status(409)
         .json({ status: "failed", message: "email already exists!" });
     } else {
-      res
-        .status(500)
-        .json({
-          status: "failed",
-          message: "Unable To Register",
-          error: error.message,
-        });
+      res.status(500).json({
+        status: "failed",
+        message: "Unable To Register",
+        error: error.message,
+      });
     }
   }
 };
 
+// user login
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (email && password) {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (!passwordMatch) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+      const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+        expiresIn: process.env.EXPIRES_IN
+      });
+      return res.status(200).json({
+        message: "sign in Successful",
+        data: {
+          token: token,
+        }
+      });
+    } else {
+      res
+        .status(400)
+        .json({ status: "failed", message: "All fields are required" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export default {
   registerUser,
+  userLogin,
 };
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ import userController from '../controllers/userController.js';
 
 const router = express.Router();
 router.post("/register", userController.registerUser);
+router.post("/login", userController.userLogin);
 
 export default router;
