cf19a7baec9705a752398ed066bee7f9770e9956
Vivek <vivekraj78965.vr@gmail.com>
2023-12-23 14:54:32 +0530

    added security packages

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ import router from './routes/index.js';
 import passport from 'passport';
 import cors from 'cors';
 
+//security packages
+import helmet from 'helmet';
+import xss_clean from 'xss-clean';
+import mongoSanitize from 'express-mongo-sanitize'; 
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 //middlewares
+app.use(helmet());          // secure our header section
+app.use(xss_clean());      // checks and sanitize the input coming from POST, GET, params
+app.use(mongoSanitize()); // middleware which sanitizes user-supplied data to prevent MongoDB Operator Injection from(req.body, req.query or req.params).
 app.use(cors({origin: '*'}));
 app.use(passport.initialize());
 app.use(express.json());
